e15775f722191b3f7c010d0c798a6d6020e66b86
srisri96 <srikanthsitharth80@gmil.com>
2022-07-22 18:07:38 +0530

    Contact edited

---
+++ EditContact/EditContact/Address.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditContact
+{
+    internal class Address
+    {
+        List<Contacts> add_book = new List<Contacts>();
+
+        public void create()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter your first name");
+            contacts.first_name = Console.ReadLine();
+            Console.WriteLine("Enter your second name");
+            contacts.s_name = Console.ReadLine();
+            Console.WriteLine("Enter your address");
+            contacts.address = Console.ReadLine();
+            Console.WriteLine("Enter your city");
+            contacts.city = Console.ReadLine();
+            Console.WriteLine("Enter your state");
+            contacts.state = Console.ReadLine();
+            Console.WriteLine("Enter your zip code");
+            contacts.zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your phone number");
+            contacts.phone_no = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter your email");
+            contacts.email = Console.ReadLine();
+            add_book.Add(contacts);
+        }
+        public void edit(String Fname, String Sname) //Editing a contact
+        {
+            Contacts contact = new Contacts();
+            foreach (var person in add_book)
+            {
+                if (person.first_name.Equals(Fname) && person.s_name.Equals(Sname))
+                {
+                    Console.WriteLine("Edit and confirm the details now -");
+                    Console.WriteLine("Enter your first name");
+                    person.first_name = Console.ReadLine();
+                    Console.WriteLine("Enter your second name");
+                    person.s_name = Console.ReadLine();
+                    Console.WriteLine("Enter your address");
+                    person.address = Console.ReadLine();
+                    Console.WriteLine("Enter your city");
+                    person.city = Console.ReadLine();
+                    Console.WriteLine("Enter your state");
+                    person.state = Console.ReadLine();
+                    Console.WriteLine("Enter your zip code");
+                    person.zip = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter your phone number");
+                    person.phone_no = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter your email");
+                    person.email = Console.ReadLine();
+                    Console.WriteLine();
+                }
+            }
+        }
+        public void display()               //displaying details of each conatact
+        {
+            foreach (var data in add_book)
+            {
+                Console.WriteLine("Contacts stored_________");
+                Console.WriteLine($"Contacts No. - {data}");
+                Console.WriteLine($"Name - {data.first_name} {data.s_name}");
+                Console.WriteLine($"Address - {data.address}");
+                Console.WriteLine($"City- {data.city}");
+                Console.WriteLine($"State - {data.state}");
+                Console.WriteLine($"Zip - {data.zip}");
+                Console.WriteLine($"Phone no. - {data.phone_no}");
+                Console.WriteLine($"Email - {data.email}\n");
+            }
+        }
+    }
+}
---
+++ EditContact/EditContact/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Welcome to Address Book Program in Address Book Main class on Master Branch");
+Console.WriteLine("Press 1 to add contact Press 2 to edit contact");
+Address obj = new Address();
+int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+        Console.WriteLine("Enter the number of contacts needed to add");
+        int n = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Enter the details to create a contact");
+            obj.create();
+        }
+        Console.WriteLine("These are the contacts stored");
+        obj.display();
+        break;
+
+    case 2:
+        Console.WriteLine("Enter the first name of the contact you want to edit the details of");
+        string Firstname = Console.ReadLine();
+        Console.WriteLine("Enter the second name of the contact you want to edit the details of");
+        string s_name = Console.ReadLine();
+        obj.edit(Firstname, s_name);
+        obj.display();
+        break;
+}
